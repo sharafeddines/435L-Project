e7a67ac72b57caac79d1a88ebff5d917cbe8ade6
sharafeddines <sas106@mail.aub.edu>
2024-12-01 16:09:28 +0200

    Added documentation for review service

--- sales_service/sales_service.py
+++ sales_service/sales_service.py
@@ -3,6 +3,15 @@ from controllers.sales_controller import sales_bp, limiter
 
 from utils.database import db, init_db
 def create_app():
+    """
+    Create and configure the Flask application.
+
+    This function initializes the database, sets up necessary configurations, 
+    and registers the blueprints for handling routes.
+
+    :return: The configured Flask application instance.
+    :rtype: flask.Flask
+    """
     app = Flask(__name__)
 
     print("attempting connection")
@@ -14,6 +23,12 @@ def create_app():
     return app
 
 if __name__ == '__main__':
+    """
+    Entry point for running the Flask application.
+
+    This block initializes the Flask app, sets up rate-limiting, 
+    and starts the development server on host `0.0.0.0` with debug mode enabled.
+    """
     app = create_app()
     limiter.init_app(app)
     app.run(debug=True, host ="0.0.0.0")
